7abf366053bf9284a538f28a49613984052f9d66
vpsur <victor.ramirez.munoz.al@iespoligonosur.org>
2025-11-25 14:34:06 +0100

    prueba-casa

--- conectar.py
+++ conectar.py
@@ -3,3 +3,4 @@ import redis
 # Conexión a Redis
 conexionRedis = redis.ConnectionPool(host='localhost', port=6379, db=0, decode_responses=True)
 baseDatos = redis.Redis(connection_pool=conexionRedis)
+#prueba
